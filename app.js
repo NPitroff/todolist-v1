c31a5a3cfe10e368e69ab9e806cdf8f510e8e37e
NPitroff <nicolai.pitroff@gmail.com>
2022-03-05 19:58:21 -0800

    Changed the res commands

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ app.get("/", function(req, res){
   // logic to find the date
   var today = new Date();
   if (today.getDay() === 6 || 0){
-    res.send("Congrats! Its the Weekend!");
+    res.write("<h1>Congrats! Its the Weekend!</h1>");
   } else {
-    res.send("Drat, still the work week eh?")
+    res.write("<p>You gotta get to work!</p>");
+    res.write("<h1>Drat, still the work week eh?</h1>");
+    res.send();
   }
 
 });
