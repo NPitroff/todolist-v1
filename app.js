ee11e8055dbe56d4e51dadbeb1e869eb6d0cf1cf
NPitroff <nicolai.pitroff@gmail.com>
2022-03-10 09:18:46 -0800

    Added a switch statement

--- app.js
+++ app.js
@@ -11,6 +11,35 @@ app.get("/", function(req, res){
   var today = new Date();
   var currentDay = today.getDay();
   var day = "";
+
+// switch statement
+switch (currentDay) {
+  case 0:
+    day = "Sunday"
+    break;
+  case 1:
+    day = "Monday"
+    break;
+  case 2:
+    day = "Tuesday"
+    break;
+  case 3:
+    day = "Wednesday"
+    break;
+  case 4:
+    day = "Thursday"
+    break;
+  case 5:
+    day = "Friday"
+    break;
+  case 6:
+    day = "Saturday"
+    break;
+  default:
+  console.log("Day not Found...How?")
+}
+
+  // else/if logic
   if (currentDay === 0){
     day = "Sunday";
   } else if(currentDay === 1) {
