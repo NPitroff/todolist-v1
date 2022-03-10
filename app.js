f817069bfb6610dba3dac587afbfb1f1b3bab904
NPitroff <nicolai.pitroff@gmail.com>
2022-03-10 09:13:11 -0800

    Added else if logic to show specific day

--- app.js
+++ app.js
@@ -9,12 +9,22 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
   // logic to find the date
   var today = new Date();
-  var currentDay = today.getDate();
+  var currentDay = today.getDay();
   var day = "";
-  if (currentDay === 6 || 0){
-    day = "Weekend";
-  } else {
-    day = "Weekday";
+  if (currentDay === 0){
+    day = "Sunday";
+  } else if(currentDay === 1) {
+    day = "Monday";
+  } else if(currentDay === 2) {
+    day = "Tuesday";
+  } else if(currentDay === 3) {
+    day = "Wednesday";
+  } else if(currentDay === 4) {
+    day = "Thursday";
+  } else if(currentDay === 5) {
+    day = "Friday";
+  } else if(currentDay === 6) {
+    day = "Saturday";
   }
     res.render("list", {kindOfDay: day});
 });
