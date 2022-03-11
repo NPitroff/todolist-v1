42096cebd49a0d98f882de52b535049a0243ad0b
NPitroff <nicolai.pitroff@gmail.com>
2022-03-11 07:44:48 -0800

    Removed if/else statements

--- app.js
+++ app.js
@@ -39,22 +39,22 @@ switch (currentDay) {
   console.log("Day not Found...How?")
 }
 
-  // else/if logic
-  if (currentDay === 0){
-    day = "Sunday";
-  } else if(currentDay === 1) {
-    day = "Monday";
-  } else if(currentDay === 2) {
-    day = "Tuesday";
-  } else if(currentDay === 3) {
-    day = "Wednesday";
-  } else if(currentDay === 4) {
-    day = "Thursday";
-  } else if(currentDay === 5) {
-    day = "Friday";
-  } else if(currentDay === 6) {
-    day = "Saturday";
-  }
+  // // else/if logic
+  // if (currentDay === 0){
+  //   day = "Sunday";
+  // } else if(currentDay === 1) {
+  //   day = "Monday";
+  // } else if(currentDay === 2) {
+  //   day = "Tuesday";
+  // } else if(currentDay === 3) {
+  //   day = "Wednesday";
+  // } else if(currentDay === 4) {
+  //   day = "Thursday";
+  // } else if(currentDay === 5) {
+  //   day = "Friday";
+  // } else if(currentDay === 6) {
+  //   day = "Saturday";
+  // }
     res.render("list", {kindOfDay: day});
 });
 
