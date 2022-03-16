93d7c58fd2ec730cc5c8c46e3f1ecd013af628df
NPitroff <nicolai.pitroff@gmail.com>
2022-03-15 20:46:36 -0700

    Created options object to use JS to show date

--- app.js
+++ app.js
@@ -9,35 +9,42 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
   // logic to find the date
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  // var currentDay = today.getDay();
+  // var day = "";
 
-// switch statement
-switch (currentDay) {
-  case 0:
-    day = "Sunday"
-    break;
-  case 1:
-    day = "Monday"
-    break;
-  case 2:
-    day = "Tuesday"
-    break;
-  case 3:
-    day = "Wednesday"
-    break;
-  case 4:
-    day = "Thursday"
-    break;
-  case 5:
-    day = "Friday"
-    break;
-  case 6:
-    day = "Saturday"
-    break;
-  default:
-  console.log("Day not Found...How?")
+var options = {
+  weekday: 'long',
+  day: 'numeric',
+  month: 'long'
 }
+
+
+// // switch statement
+// switch (currentDay) {
+//   case 0:
+//     day = "Sunday"
+//     break;
+//   case 1:
+//     day = "Monday"
+//     break;
+//   case 2:
+//     day = "Tuesday"
+//     break;
+//   case 3:
+//     day = "Wednesday"
+//     break;
+//   case 4:
+//     day = "Thursday"
+//     break;
+//   case 5:
+//     day = "Friday"
+//     break;
+//   case 6:
+//     day = "Saturday"
+//     break;
+//   default:
+//   console.log("Day not Found...How?")
+// }
 
   // // else/if logic
   // if (currentDay === 0){
